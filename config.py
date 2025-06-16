39fcd126f541ea522a0533666abaa12c602aa24e
asmaradian <asmaradian.putra@gmail.com>
2025-06-16 14:09:23 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Token API Telegram (jaga kerahasiaannya, jangan upload ke GitHub publik!)
+TELEGRAM_API_TOKEN = "7126717961:AAFz9fwffUPXlbAg3LqK9-zBF11hmI95KDw"
+
+# Chat ID target (bisa channel, grup, atau user)
+TELEGRAM_CHAT_ID = "6778588870"
+
+# Daftar saham LQ45 statis dari Yahoo Finance (.JK = Bursa Efek Indonesia)
+LQ45_TICKERS = [
+    "BBCA.JK", "TLKM.JK", "BBRI.JK", "BMRI.JK", "ASII.JK", "UNVR.JK", "PGAS.JK", "TINS.JK",
+    "MDKA.JK", "ANTM.JK", "ICBP.JK", "INDF.JK", "ADRO.JK", "BRPT.JK", "CPIN.JK", "EXCL.JK",
+    "GGRM.JK", "HMSP.JK", "KLBF.JK", "MEDC.JK", "MIKA.JK", "SMGR.JK", "TKIM.JK", "WIKA.JK",
+    "WSKT.JK", "BBTN.JK", "BFIN.JK", "BUKA.JK", "ELSA.JK", "ERAA.JK", "INDY.JK", "JPFA.JK",
+    "MNCN.JK", "PTPP.JK", "SCMA.JK", "SIDO.JK", "SMRA.JK", "TBIG.JK", "TOWR.JK", "UNTR.JK",
+    "WIIM.JK", "ZINC.JK"
+]
